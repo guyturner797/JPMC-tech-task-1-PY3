9f7a23facf0d8f0e668683de32dfe5d8386535e5
Guy Turner <guyturner797@gmail.com>
2020-06-07 19:36:45 +0100

    Create Patch File

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +17,22 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+        print(getDataPoint(quote))
+        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
+  # Test to ensure correct ratio is calculated for normal data
+  def test_getRatio_CalculateRatio(self):
+      self.assertEqual(getRatio(12.0, 6.0), 2.0)
 
+  # Test to ensure when A price is 0 a ratio of 0 is returned
+  def test_getRatio_AZero(self):
+      self.assertEqual(getRatio(0, 119.2), 0)
+
+  # Test to ensure when B price is 0 a ZeroDivisionError is not raised
+  def test_getRatio_BZero(self):
+      self.assertIsNone(getRatio(119.2, 0))
 
 
 if __name__ == '__main__':
